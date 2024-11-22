fe3df3eb268889b6d3515d126498df35145e754a
Mehul Singh <mehulsingh2022@vitbhopal.ac.in>
2024-11-22 15:13:03 +0530

    clerk setup

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,26 +1,23 @@
 import localFont from "next/font/local";
 import "./globals.css";
-import {Outfit} from "next/font/google"
+import { Outfit } from "next/font/google";
+import { ClerkProvider } from "@clerk/nextjs";
 
-
-const outfit = Outfit({subsets:['latin']});
-
+const outfit = Outfit({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Learning LMS app",
+  title: "Topper Stack",
   description: "Study and excel",
 };
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body
-        className={outfit.className}
-        suppressHydrationWarning
-      >
-        
-        {children}
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={outfit.className} suppressHydrationWarning>
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,10 @@
-import Image from "next/image";
+import { UserButton } from "@clerk/nextjs"
 
 export default function Home() {
   return (
     <div>
       <h2 className="text-primary">Hello world</h2>
+      <UserButton />
     </div>
   );
 }
